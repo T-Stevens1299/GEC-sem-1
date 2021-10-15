87e35c6648f5796d55d5be8383880b09c7888488
T-Stevens1299 <91890549+T-Stevens1299@users.noreply.github.com>
2021-10-15 11:28:33 +0100

    Week2Part2

---
+++ Program3_definingVariables/Program3_definingVariables.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int Int1 = 5, Int2;
+    Int2 = 10;
+
+    cout << "Value stored in Int1 is " << Int1 << endl;
+    cout << "Value stored in Int2 is " << Int2 << endl;
+
+
+    float Float1 = 1.2f, Float2;
+    Float2 = 9.9f;
+
+    cout << "Value stored in Float1 is " << Float1 << endl;
+    cout << "Value stored in Float2 is " << Float2 << endl;
+
+    char Char1 = 'a';
+    cout << "Value stored in Char1 is " << Char1 << endl;
+
+    return 0;
+}
