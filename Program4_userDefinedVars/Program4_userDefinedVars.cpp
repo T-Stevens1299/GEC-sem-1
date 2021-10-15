5547f751b613f3a0cf5d3ffcd63719007b9134a9
T-Stevens1299 <91890549+T-Stevens1299@users.noreply.github.com>
2021-10-15 11:39:43 +0100

    Week2Part3

---
+++ Program4_userDefinedVars/Program4_userDefinedVars.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    typedef int Health;
+
+    enum Weapon
+    {
+        Sword = 1,
+        Dagger,
+        Mace,
+        Twin_Swords,
+        Samurai,
+        Wizard_Staff,
+        Fire_Potion,
+        Ice_Blade,
+        Small_Knife,
+    };
+
+    Weapon CurrentWeapon = Samurai;
+    Health CurrentHealth = 13000;
+
+    cout << "My current health is " << CurrentHealth << endl;
+    cout << "And the ID of my weapon of choice is " << CurrentWeapon << endl;
+
+}
+
