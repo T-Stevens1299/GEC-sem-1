e17c3a1babb3c5a242a0aedf739bb010fd4005fc
T-Stevens1299 <91890549+T-Stevens1299@users.noreply.github.com>
2021-10-29 12:50:12 +0100

    Update Program15_StarryOutput.cpp

--- Program15_StarryOutput/Program15_StarryOutput.cpp
+++ Program15_StarryOutput/Program15_StarryOutput.cpp
@@ -6,17 +6,21 @@ int main()
 	int NoOfRows;
 	bool cont = true;
 	int UInput;
+
 	while (cont == true)
 	{
 
 		cout << "Please enter a number between 1 and 10" << endl;
 		cin >> NoOfRows;
-		for (int i = 0; i < NoOfRows; i++)
+		for (int i = 0; i <= NoOfRows; i++)
 		{
-			cout << " * " << endl;
-			cout << " * " << ends;
-			
+			for (int j = 0; j < i; j++)
+			{
+				cout << " * ";
+			}
+			cout << "\n";
 		}
+
 		cout << "Do you want to continue? 1 for yes, 0 for no" << endl;
 		cin >> UInput;
 
