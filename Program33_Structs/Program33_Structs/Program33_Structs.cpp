c02733364ebc3c593f771b4c04dc39ef06323043
T-Stevens1299 <91890549+T-Stevens1299@users.noreply.github.com>
2021-11-26 12:55:30 +0000

    Week8Part4

---
+++ Program33_Structs/Program33_Structs/Program33_Structs.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    struct personDetails
+    {
+        string name;
+        string phoneNumber;
+        int age;
+    };
+
+    personDetails.PrintDetails();
+
+}
