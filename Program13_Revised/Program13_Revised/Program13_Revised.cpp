c15fbf6da109f3305ca3143559430a45f42d5e8c
T-Stevens1299 <s012473l@student.staffs.ac.uk>
2022-01-17 20:00:34 +0000

    Program13_Revised

---
+++ Program13_Revised/Program13_Revised/Program13_Revised.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int NoOfRows;
+	bool cont = true;
+	int UInput;
+
+	while (cont == true)
+	{
+		
+		cout << "Please enter a number between 1 and 10" << endl;
+		cin >> NoOfRows;
+
+		while (NoOfRows < 0 || NoOfRows > 10)
+		{
+			cout << "That number was not between 1 and 10, please enter another number" << endl;
+			cin >> NoOfRows;
+		}
+
+		for (int i = 0; i <= NoOfRows; i++)
+		{
+			for (int j = 0; j < i; j++)
+			{
+				cout << " * ";
+			}
+			cout << "\n";
+		}
+
+		cout << "Do you want to continue? 1 for yes, 0 for no" << endl;
+		cin >> UInput;
+
+		if (UInput == 1)
+		{
+			cont = true;
+
+
+		}
+		else if (UInput == 0)
+		{
+			cont = false;
+			cin.get();
+		}
+	}
+	
+}
